36708dd9a647ff1602bfba6d7566ecb85a6c9a34
xxxagent350 <54989881+xxxagent350@users.noreply.github.com>
2024-06-11 14:13:56 +0300

    Revert "лазер начал"
    This reverts commit 2e67b5d2325b39eb494cf7520ae4a562bbe518cc.

--- Assets/Scripts/Модули/LaserWeapon.cs
+++ Assets/Scripts/Модули/LaserWeapon.cs
@@ -1,12 +1,9 @@
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class LaserWeapon : Weapon
 {
-    private void OnTriggerEnter2D()
     public override void FixedServerUpdate()
     {
         //FixedUpdate, выполн€ющийс€ на сервере
-        
     }
 }
